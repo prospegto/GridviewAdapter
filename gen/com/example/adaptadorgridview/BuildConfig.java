eca0d2614f8a6417aec555fcc3ca0a05d152cb24
Ignacio <nacho.prosper@gmail.com>
2015-12-11 12:54:03 +0000

    Up

---
+++ gen/com/example/adaptadorgridview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.adaptadorgridview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/adaptadorgridview/Titulos.java
@@ -0,0 +1,21 @@
+package com.example.adaptadorgridview;
+
+public class Titulos {
+	
+	private String titulo;
+    private String subtitulo;
+ 
+    public Titulos (String titulo, String subtitulo) {
+        this.titulo = titulo;
+        this.subtitulo = subtitulo;
+    }
+ 
+    public String getTitulo(){
+        return titulo;
+    }
+ 
+    public String getSubtitulo(){
+        return subtitulo;
+    }
+    
+}
